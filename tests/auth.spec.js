70582f6c56af512ad0230d2c815f9486b3953daa
SanaSanat <sst7212@gmail.com>
2023-08-29 00:11:17 -0700

    add auth.spec.js

--- tests/auth.spec.js
+++ tests/auth.spec.js
@@ -1,7 +1,13 @@
 const { test, expect } = require ('@playwright/test');
 
 test.describe('Authentication & Authorization', () => {
-   test('Sign in with existing credentials', async () => {
+   test('Sign in with existing credentials', async ({ page }) => {
+     await page.goto('https://coding.pasv.us/user/login')
 
+     await page.locator('#normal_login_email').fill('sst7212@gmail.com')
+     await page.locator('#n#normal_login_password').fill('zhailayY2202')
+     await page.locator('button[type="submit"]').click()
+
+     await expect(page.locator('.ant-avatar-square')).toBeVisible()
    })
 })
