a0a424daa9978f8d3357fe7df49e24a002b0d897
SanaSanat <sst7212@gmail.com>
2023-08-28 22:25:52 -0700

    first commit

---
+++ tests/auth.spec.js
@@ -0,0 +1,7 @@
+const { test, expect } = require ('@playwright/test');
+
+test.describe('Authentication & Authorization', () => {
+   test('Sign in with existing credentials', async () => {
+
+   })
+})
