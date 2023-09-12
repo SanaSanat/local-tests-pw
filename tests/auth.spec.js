2319ed37907d813c6da105ed5cd42fb1aa9056f1
SanaSanat <sst7212@gmail.com>
2023-09-11 22:27:18 -0700

    update auth.spec,add login.js

--- tests/auth.spec.js
+++ tests/auth.spec.js
@@ -1,27 +1,29 @@
-const {test, expect} = require('@playwright/test')
+import {test, expect} from '@playwright/test'
+import LoginPage from '../pages/login'
 
 test.describe('Authentication & Authorization', () => {
+  let loginPage
+
   test.beforeEach(async ({page}) => {
-    await page.goto('/user/login')
+    //await page.goto('/user/login')
+    loginPage = new LoginPage(page)
+    await loginPage.open()
   })
 
   test('Sign in with existing credentials', async ({page}) => {
-    await page.locator('#normal_login_email').fill(process.env.EMAIL)
-    await page.locator('#normal_login_password').fill(process.env.PASSWORD)
-    await page.locator('button[type="submit"]').click()
+    await loginPage.inputEmail.fill(process.env.EMAIL)
+    await loginPage.inputPassword.fill(process.env.PASSWORD)
+    await loginPage.buttonSubmit.click()
 
     await expect(page.locator('.ant-avatar-square')).toBeVisible()
   })
 
   test('Sign in with not existing credentials', async ({page}) => {
-    await page.goto('https://coding.pasv.us/user/login')
+    await loginPage.inputEmail.fill('invalid@gmail.com')
+    await loginPage.inputPassword.fill('invalid')
+    await loginPage.buttonSubmit.click()
 
-    await page.locator('#normal_login_email').fill('invalid@gmail.com')
-    await page.locator('#normal_login_password').fill('invalid')
-    await page.locator('button[type="submit"]').click()
-
-    const toast = page.locator('.ant-notification-notice-message')
-    await expect(toast).toBeVisible()
-    await expect(toast).toHaveText('User login. Fail')
+    await expect(loginPage.toast).toBeVisible()
+    await expect(loginPage.toast).toHaveText('User login. Fail')
   })
 })
