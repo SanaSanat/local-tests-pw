14e51ef9d2834eb388327c0e4e447722907c8ee2
SanaSanat <sst7212@gmail.com>
2023-09-23 22:02:52 -0700

    update page.js, test.js, add profile.spec.js, profile.js

--- common/test.js
+++ common/test.js
@@ -1,10 +1,14 @@
 import {test as base, expect} from '@playwright/test'
 import LoginPage from '../pages/login'
+import ProfilePage from '../pages/profile'
 
 const test = base.extend({
   loginPage: async ({page}, use) => {
     await use(new LoginPage(page))
   },
+  profilePage: async ({page}, use) => {
+    await use(new ProfilePage(page))
+  }
 })
 
 export {test, expect}
